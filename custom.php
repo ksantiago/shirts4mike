3ec8b5212728eac83e79804dc75ed4158cc71ced
Katherine Santiago <santiago.kat@gmail.com>
2014-03-17 13:43:56 -0400

    first commit

---
+++ custom.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Shirts 4 Mike</title>
+</head>
+<body>
+  <h1>Shirts 4 Mike</h1>
+  <p>&copy;<?php echo date("Y"); ?> Shirts 4 Mike</p>
+
+</body>
+</html>
+
+<h1><?php echo "Shirts 4 Mike"; ?></h1>
